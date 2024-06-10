eef7447d7d8ca524ad4b4816674f6c0cf2d222f4
Anabel <anabel.pmoraes@gmail.com>
2024-06-10 17:08:53 -0300

    Added more tokens.

--- token/token.go
+++ token/token.go
@@ -1,11 +1,12 @@
 package token
 
-type TokenType string
-
-type Token struct {
-	Type    TokenType
-	Literal string
-}
+type (
+	TokenType string
+	Token     struct {
+		Type    TokenType
+		Literal string
+	}
+)
 
 const (
 	ILLEGAL = "ILLEGAL"
@@ -32,3 +33,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
